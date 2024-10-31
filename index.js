d5ac684eb370b441f5ac748db704f9825fb172a4
Abdelrahman-AboELshohoud <alaaeldinafghani@gmail.com>
2024-10-31 19:01:17 +0300

    fixing an error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const compression = require("compression");
 const helmet = require("helmet");
 const authRouter = require("./routes/user.route");
-const { default: mongoose } = require("mongoose");
+const { mongoose } = require("mongoose");
 
 app.use(compression());
 app.use(helmet());
@@ -14,17 +14,19 @@ app.use(express.json());
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.log(err, 'error in db connecting');
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err, "error in db connecting");
+  });
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use("/auth", authRouter);
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
